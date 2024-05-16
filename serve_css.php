d219624243a77c681e0117be6bb081bb67b48283
Andy Kirk <andy@andykirk.net>
2024-05-16 16:58:06 +0100

    J4

--- serve_css.php
+++ serve_css.php
@@ -1,5 +1,9 @@
 <?php
 
+use Joomla\CMS\Factory;
+#use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Registry\Registry;
+
 if (empty($_GET['mod_id'])) {
     die;
 }
@@ -17,38 +21,72 @@ if (!is_numeric($_GET['mod_id'])) {
 //}
 
 
+// Set up Joomla User stuff:
+define('DS', DIRECTORY_SEPARATOR);
+$base_path = realpath(dirname(dirname(__DIR__)));
+define('BASE_PATH', $base_path . DS);
+#echo "<pre>"; var_dump(BASE_PATH); echo "</pre>"; exit;
 
-//init Joomla Framework
 define('_JEXEC', 1);
-define('JPATH_BASE', realpath(dirname(dirname(__DIR__))) . '/administrator');
 
+//If this file is not placed in the /root directory of a Joomla instance put the directory for Joomla libraries here.
+$joomla_directory = BASE_PATH;
 
+// From https://joomla.stackexchange.com/questions/33140/how-to-create-an-instance-of-the-joomla-cms-from-the-browser-or-the-command-line
+// Via: https://joomla.stackexchange.com/questions/33389/standalone-php-script-to-get-username-in-joomla-4
+/**---------------------------------------------------------------------------------
+ * Part 1 - Load the Framework and set up up the environment properties
+ * -------------------------------------------------------------------------------*/
 
+/**
+ *  Site - Front end application when called from Browser via URL.
+*/                                                  // Remove this '*/' to comment out this block
+define('JPATH_BASE', (isset($joomla_directory)) ? $joomla_directory : __DIR__ );
 require_once JPATH_BASE . '/includes/defines.php';
 require_once JPATH_BASE . '/includes/framework.php';
+$class_name             =  new \Joomla\CMS\Application\SiteApplication;
+$session_alias          = 'session.web';
+$session_suffix         = 'web.site';
+/** end Site config */
 
-$app     = JFactory::getApplication('administrator');
+/**---------------------------------------------------------------------------------
+ * Part 2 - Start the application from the container ready to be used.
+ * -------------------------------------------------------------------------------*/
+// Boot the DI container
+$container = \Joomla\CMS\Factory::getContainer();
 
-$app->initialise(null, false);
+// Alias the session service key to the web session service.
+$container->alias($session_alias, 'session.' . $session_suffix)
+          ->alias('JSession', 'session.' . $session_suffix)
+          ->alias(\Joomla\CMS\Session\Session::class, 'session.' . $session_suffix)
+          ->alias(\Joomla\Session\Session::class, 'session.' . $session_suffix)
+          ->alias(\Joomla\Session\SessionInterface::class, 'session.' . $session_suffix);
+
+// Instantiate the application.
+$app = $container->get($class_name::class);
+// Set the application as global app
+\Joomla\CMS\Factory::$application = $app;
 
 
-$db = JFactory::getDbo();
+
+$db = Factory::getDbo();
+
 $query = $db->getQuery(true);
 $query->select('m.*');
 $query->from('#__modules AS m');
-$query->where('id = '.JRequest::getInt('mod_id'));
+$query->where('id = ' . $_GET['mod_id']);
+//$query->where('id = ' . Request::getInt('mod_id'));
 $db->setQuery($query);
 $module = $db->loadObject();
-$params = new JRegistry($module->params);
+$params = new Registry;
+$params->loadString($module->params);
 
-
-//$module = JModuleHelper::getModule('mod_infographic', urldecode($_GET['mod_title']));
-//$params = new JRegistry($module->params);
 #echo '<pre>'; var_dump($params); echo '</pre>';exit;
 
 #$css = base64_decode(urldecode($_GET['c']));
 #$css = html_entity_decode(base64_decode($_GET['c']));
 $css = $params->get('css_file');
+
 header('Content-type: text/css');
 echo $css;
 exit;
